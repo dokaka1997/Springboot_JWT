e0c12eb28292f96325ea7066f1d6e1a28bdcc407
dokaka1997@gmail.com <Dontsailam1997>
2021-07-16 13:31:53 +0700

    init

---
+++ src/main/java/com/dvdo/service/JwtUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.dvdo.service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import com.dvdo.model.entity.UserEntity;
+import com.dvdo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    UserRepository userRepository;
+
+    @Autowired
+    public JwtUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserEntity> userEntity = userRepository.getByUsername(username);
+
+        if (userEntity.isPresent()) {
+            String password = "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6";
+            return new User(userEntity.get().getUsername(), password,
+                    new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+    }
+
+}
