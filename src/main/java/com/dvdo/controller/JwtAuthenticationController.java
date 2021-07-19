cec0e2154012f14133598479cbb399c126c4ba52
dokaka1997@gmail.com <Dontsailam1997>
2021-07-19 08:13:32 +0700

    init

--- src/main/java/com/dvdo/controller/JwtAuthenticationController.java
+++ src/main/java/com/dvdo/controller/JwtAuthenticationController.java
@@ -1,15 +1,10 @@
 package com.dvdo.controller;
 
+import com.dvdo.model.request.JwtRequest;
 import com.dvdo.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.dvdo.model.request.JwtRequest;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
